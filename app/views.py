63a57c75a1d4d3a47b4442c8f737174ce76e4bf4
Bryan Ng <bryan.yc.ng@shaw.ca>
2016-12-03 12:35:30 -0800

    added basic template

--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.template import loader
+import requests
 
 # Create your views here.
+def index(request):
+    return render(request, 'scheduler/index.html')
